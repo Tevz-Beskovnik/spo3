e8d91ef449b722f4d40eef2faee9ae0aa087354a
Tevz-Beskovnik <tevz.beskovnik@gmail.com>
2025-03-28 18:16:39 +0100

    final

--- Makefile
+++ Makefile
@@ -3,9 +3,10 @@ LD=ld
 
 all: build run
 
+# če fali /usr/lib32/libc.so.6 installiraj lib32-glibc na archu :P
 build:
 	$(AS) -felf32 main.asm -o main.o
-	$(LD) -m elf_i386 main.o /usr/lib/libc.so.6 -I/usr/lib/ld-linux.so.2 -o main
+	$(LD) -m elf_i386 main.o /usr/lib32/libc.so.6 -I/usr/lib/ld-linux.so.2 -o main
 
 run: 
 	@./main
--- main.asm
+++ main.asm
@@ -1,23 +1,27 @@
+; mov [spremenljivka_v_bss],eax <-- shranimo vrednost na naslovu od spremenljivke
+; klicanje printfa
+; psush ecx <-- vrednost
+; push format_string <--- naslov format stringa
+; call printf <-- klic funkcije
+; add esp, 8 <-- argumenta sta zavzela 2x po 4 bajte ker smo v 32-bitnem modu oz smo uporabili 32 bitne registre
+
 ; Define variables in the data section
 SECTION .DATA ; section that creates initialized variables
-	hello:     db 'Hello world!',10
-	helloLen:  equ $-hello
     sysdir:    db 'SysDir',0
-    sysdirLen: equ $-sysdir
     filename:  db 'PidTimeData.dat',0
+    format:    db '%d %d',10,0
 
-SECTION .BSS ; section that reserves data for variables (uninitialized)
+SECTION .bss; section that reserves data for variables (uninitialized data)
+    fd         resd  1
+    pid        resd  1
+    time       resd  1
 
 ; Code goes in the text section
 SECTION .TEXT
 	GLOBAL _start 
+    extern dprintf
 
 _start:
-;	mov eax,4            ; 'write' system call = 4
-;	mov ebx,1            ; file descriptor 1 = STDOUT
-;	mov ecx,hello        ; string to write
-;	mov edx,helloLen     ; length of string to write
-;	int 80h              ; call the kernel
     mov eax,0x27         ; mkdir syscal
     mov ebx,sysdir
     mov ecx,0o764        ; rwxrw-r--
@@ -27,22 +31,33 @@ _start:
     mov ebx,sysdir
     int 0x80
 
-; mov [spremenljivka_v_bss],eax <-- shranimo vrednost na naslovu od spremenljivke
-; klicanje printfa
-; psush ecx <-- vrednost
-; push format_string <--- naslov format stringa
-; call printf <-- klic funkcije
-; add esp, 8 <-- argumenta sta zavzela 2x po 4 bajte ker smo v 32-bitnem modu oz smo uporabili 32 bitne registre
-    mov eax,0x08         ; create
+    mov eax,0x05         ; open
     mov ebx,filename
-    mov ecx,0o640        ; rw-r-----
+    mov ecx,2|64|512
+    mov edx,0o640        ; rw-r-----
+    int 0x80
+    mov [fd],eax         ; shrani fd v fd spremenljivko
+    
+    mov eax,0x14         ; getpid
+    int 0x80
+    mov [pid],eax        ; store pid into pid var
+
+    mov eax,0x0d         ; time
+    mov ebx,time         ; addres to time variable
     int 0x80
 
-    mov eax,0x05         ; open
+    push dword[time]     ; store the value of time as last arg
+    push dword[pid]      ; store the value of pid as arg
+    push format          ; store ptr to format string as arg
+    push dword[fd]       ; store value of fd as arg
+    call dprintf
+    add esp,16           ; move stack pointer back 16 to free space that args have taken up
 
-	; Terminate program
-	mov eax,1            ; 'exit' system call
-    xor ebx,ebx            ; exit with error code 0
-	int 0x80              ; call the kernel
+    mov eax,0x06
+    mov ebx,[fd]
+    int 0x80
 
+	mov eax,1            ; exit
+    xor ebx,ebx          ; exit code 0
+	int 0x80
 
